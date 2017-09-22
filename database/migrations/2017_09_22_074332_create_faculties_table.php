fd3fde448563753f5d5e220012e7ed5251afc85a
NEYUGNYUD <duypeo96@gmail.com>
2017-09-22 14:47:31 +0700

    tạo migration bang faculties

---
+++ database/migrations/2017_09_22_074332_create_faculties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacultiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('faculties')) {
+            Schema::create('faculties', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 100);
+                $table->string('slug', 150);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faculties');
+    }
+}
