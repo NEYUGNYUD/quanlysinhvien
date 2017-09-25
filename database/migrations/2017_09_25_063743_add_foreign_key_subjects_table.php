2d6f2d93e6069a20be5b22f303dfe167c4fa0855
NEYUGNYUD <duypeo96@gmail.com>
2017-09-25 13:51:32 +0700

    tạo khóa ngoại bảng subjects

---
+++ database/migrations/2017_09_25_063743_add_foreign_key_subjects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeySubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('subjects', 'teacher_id')) {
+            Schema::table('subjects', function(Blueprint $table) {
+                $table->integer('teacher_id')->unsigned();
+                $table->foreign('teacher_id')->references('id')->on('teachers');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subjects', function(Blueprint $table) {
+            $table->dropColumn('teacher_id');
+            $table->dropForeign('teacher_id');
+        });
+    }
+}
