d7745bcb506048bb8fcd9e29aa96222be362db90
NEYUGNYUD <duypeo96@gmail.com>
2017-09-25 13:36:08 +0700

    thêm khoa ngoai bang teachers

---
+++ database/migrations/2017_09_22_102946_add_foreign_key_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('teachers', 'faculty_id')) {
+            Schema::table('teachers', function(Blueprint $table) {
+               $table->integer('faculty_id')->unsigned();
+               $table->foreign('faculty_id')->references('id')->on('faculties');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teachers', function(Blueprint $table) {
+            $table->dropColumn('faculty_id');
+            $table->dropForeign('teachers_faculty_id_foreign');
+        });
+    }
+}
