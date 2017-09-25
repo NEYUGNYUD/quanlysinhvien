0c01b207f2c51baa4c89880dca173d82f4860ab5
NEYUGNYUD <duypeo96@gmail.com>
2017-09-25 17:09:53 +0700

    tao model StudentSubject

---
+++ app/StudentSubject.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentSubject extends Model
+{
+    protected $table = 'students_subjects';
+
+    public function student() {
+        return $this->belongsTo('App\Student', 'student_id', 'id');
+    }
+
+    public function scores() {
+        return $this->hasMany('App\Score', 'student_subject_id', 'id');
+    }
+
+    public function subject() {
+        return $this->belongsTo('App\Subject', 'subject_id', 'id');
+    }
+}
