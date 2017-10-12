93c2f6802c730feed8fbdd3d0d55ea174b466dd7
NEYUGNYUD <duypeo96@gmail.com>
2017-10-12 16:54:46 +0700

    tao bang wards

---
+++ database/migrations/2017_10_12_165406_create_wards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasTable('wards'))
+        Schema::create('wards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('slug', 80);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wards');
+    }
+}
